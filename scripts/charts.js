f98d52d2f85b1382c34b55b73b76326b750ec287
jfaith630 <jfaith630@gmail.com>
2020-03-25 14:12:59 -0400

    HTML, CSS, & JS updated

--- scripts/charts.js
+++ scripts/charts.js
@@ -1,62 +1,44 @@
 //CHART #1
 //How do you organize your files in your cloud storage application of choice? (Please select all that apply.)
-var ctx = document.getElementById('chart-1').getContext('2d');
-var chart = new Chart(ctx, {
+var ctxOne = document.getElementById('chart-1').getContext('2d');
+var chart = new Chart(ctxOne, {
     // The type of chart we want to create
     type: 'bar',
 
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['Category/Group Tags', 'Favorite Tags', 'Folders', 'I don\'t organize my files'],
         datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            label: '# of Votes',
+            backgroundColor: ['#00B8A2', '#0AB1D6', '#B294D6', '#EF817B'],
+            borderColor: ['#00796B', '#044756', '#45266A', '#E1261C'],
+            data: [5, 1, 11, 2]
         }]
     },
 
     // Configuration options go here
-    options: {}
+    options: {
+      legend: {
+        display: false,
+      }
+    }
 });
 
 //CHART #2
-//Are you interested in accessing cloud storage applications on your smartphone or other mobile device?
-var ctx = document.getElementById('chart-2').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'pie',
-
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
-});
-
-//CHART #3
 //How important are collaborative features to you in a cloud storage application?
-var ctx = document.getElementById('chart-3').getContext('2d');
-var chart = new Chart(ctx, {
+var ctxTwo = document.getElementById('chart-2').getContext('2d');
+var chart = new Chart(ctxTwo, {
     // The type of chart we want to create
     type: 'pie',
 
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['Very Important', 'Important', 'Kind of Important', 'Not Important'],
         datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            label: '# of Votes',
+            backgroundColor: ['#00B8A2', '#0AB1D6', '#B294D6', '#EF817B'],
+            borderColor: ['#00796B', '#044756', '#45266A', '#E1261C'],
+            data: [8, 5, 2, 0]
         }]
     },
 
